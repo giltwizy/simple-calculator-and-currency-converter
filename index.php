889008de7a763880a695c5aeb1c884064283d3af
Giltwizy <giltonbosco@yahoo.com>
2020-01-11 14:34:14 +0300

    html designing

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Simple calculator in PHP</title>
+        <meta name="description" content="Simple calculator">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+        <form action="index.php" method="get">
+            <b>Number 1</b>
+            <input type="number" name="value1" placeholder="First Number">
+            <b>Number 2</b>
+            <input type="number" name="value1" placeholder="Second Number">
+            <
+            
+
+        </form>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="" async defer></script>
+    </body>
+</html>
